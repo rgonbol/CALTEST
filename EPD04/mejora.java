176234f1fc6b2738afc5913b5dc29c512422cc4e
rgonbol <rgonbol@upo.es>
2023-10-22 11:17:11 +0200

    Change related to #1 – Adding skeleton

---
+++ EPD04/mejora.java
@@ -0,0 +1,9 @@
+public class MiClase {
+    public static void main(String[] args) {
+        saludar();
+    }
+
+    public static void saludar() {
+        System.out.println("Hola, Mundo");
+    }
+}
